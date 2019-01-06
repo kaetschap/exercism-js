d5685132313bcf8c97f11ca4b58db4c668604e0a
Sonja Heins <sonja.heins@codecentric.de>
2019-01-06 20:39:19 +0100

    Solve zipper exercise

--- zipper/zipper.js
+++ zipper/zipper.js
@@ -9,6 +9,37 @@ export class Zipper {
   }
 
   toTree() {
-    return this.parent ? this.toTree(this.parent) : this.subtree;
+    return this.parent ? this.parent.toTree() : this.subtree;
+  }
+
+  value() {
+    return this.subtree.value;
+  }
+
+  left() {
+    return this.subtree.left ? new Zipper(this.subtree.left, this) : null;
+  }
+
+  right() {
+    return this.subtree.right ? new Zipper(this.subtree.right, this) : null;
+  }
+
+  up() {
+    return this.parent ? this.parent : null;
+  }
+
+  setValue(newValue) {
+    this.subtree.value = newValue;
+    return this;
+  }
+
+  setLeft(leaf) {
+    this.subtree.left = leaf;
+    return this;
+  }
+
+  setRight(leaf) {
+    this.subtree.right = leaf;
+    return this;
   }
 }
